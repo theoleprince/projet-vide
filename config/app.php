fc73dc280ee94791bf4f658e1d746334f4da3a1b
theoleprince <jiozangtheophane@gmail.com>
2020-09-01 01:05:23 +0100

    composer laraadmin

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Laraadmin Service Providers...
+         */
+        Dwij\Laraadmin\LAProvider::class,
+
     ],
 
     /*
